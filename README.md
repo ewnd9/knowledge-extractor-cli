21ca4e1c414c8d7a69af9dfbcd70cf1a6d78af75
ewnd9 <ewndnine@gmail.com>
2016-03-27 22:26:25 +0800

    init

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 [![Build Status](https://travis-ci.org/ewnd9/knowledge-extractor-cli.svg?branch=master)](https://travis-ci.org/ewnd9/knowledge-extractor-cli)
 
+Tools to explore old projects of yours in `Atom`. Knowledge must not be lost.
+
+Atom opens a file in a instance of a project so there will be only one atom window at time for the project.
+
 ## Install
 
 ```
-$ npm install knowledge-extractor-cli
+$ npm install knowledge-extractor-cli -g
 ```
 
 ## Usage
 
 ```
-INSERT_USAGE
+$ knowledge-extractor "src/**/*.js test/**/*.js"
 ```
 
 ## License
--- test/test.js
+++ test/test.js
@@ -1,6 +1,18 @@
 import test from 'ava';
 import 'babel-core/register';
 
-test('true', t => {
-  t.true(true);
+import execa from 'execa';
+
+test('open file', async t => {
+  const dir = `/tmp/${Math.random()}`;
+
+  console.log(dir);
+
+  await execa.shell(`mkdir -p ${dir}/src && cd ${dir} && git init && git remote add origin ${dir} && cd ${dir}/src && touch index.js && touch data.js`);
+
+  const { stdout: s1 } = await execa.shell(`cd ${dir} && node "${__dirname}/../cli" "**/*.js"`);
+  const { stdout: s2 } = await execa.shell(`cd ${dir} && node "${__dirname}/../cli" "**/*.js"`);
+
+  t.ok(new RegExp('2 left').test(s1));
+  t.ok(new RegExp('1 left').test(s2));
 });
